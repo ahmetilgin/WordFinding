5494f7b36c3f8e85f0332c10949c0902e0d8c6c5
ahmetilgin <ahmetilgin61@gmail.com>
2020-08-23 21:49:52 +0300

    rastgele eklendi.

--- game.gd
+++ game.gd
@@ -3,6 +3,7 @@ var block_scene = preload("res://Block.tscn")
 var frame_scene = preload("res://Frame.tscn")
 var background = preload("res://Background.tscn")
 var block_list = []
+var word_sum_list = []
 func fill_frames(position):
 	var frame = frame_scene.instance()
 	add_child(frame)
@@ -62,8 +63,11 @@ func subset_sum(numbers, target, partial=[]):
 	var s = sum_array(partial)
 
 	# check if the partial sum is equals to target
-	if s == target: 
-		print (partial, target)
+	if s == target:
+		partial.sort()
+		if len(word_sum_list) > 5:
+			return
+		word_sum_list.push_back(partial)
 	if s >= target:
 		return  # if we reach the number why bother to continue
 
@@ -76,7 +80,35 @@ func subset_sum(numbers, target, partial=[]):
 
 
 func fill_words_to_blocks():
-	subset_sum([1,3,54,6,2,7,8],9)
+	randomize()
+	Globals.words.shuffle()
+	var word_list_with_count = {}
+	var word_list_count_list = []
+	var sum_word_list = []
+	for	i in range(0,len(Globals.words)):
+		if(len(Globals.words[i]) <=  Globals.map_size):
+			var letter_count = len(Globals.words[i])
+			if !word_list_with_count.has(letter_count):
+				word_list_with_count[letter_count] = []
+			word_list_count_list.push_back(letter_count)
+			word_list_with_count[letter_count].push_back(Globals.words[i])
+			
+	subset_sum(word_list_count_list,Globals.map_size * Globals.map_size - 1)
+	if len(word_sum_list) >0 :
+		var sum_of_list_member = word_sum_list[randi() % len(word_sum_list)]
+		for word_len in range(0, len(sum_of_list_member)):
+			sum_word_list.append(word_list_with_count[sum_of_list_member[word_len]].pop_back())
+#			word_list_count_list.remove()
+	var total_letters = []
+	for word in sum_word_list:
+		var checkbox = CheckBox.new() ;
+		$ItemList.add_item(word,checkbox)
+		for letter in word:
+			total_letters.append(letter)
+	total_letters.shuffle()
+	
+	for	block in block_list:
+		block.set_label(total_letters.pop_back())
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
