2c6bd71d2faaaa970f76bc76ced6829ca7d14dbb
suattosun <sttsn23@gmail.com>
2020-09-05 01:12:36 +0300

    tween 2

--- block_kinematic.gd
+++ block_kinematic.gd
@@ -12,18 +12,23 @@ func _ready():
 	add_child(move_star)
 	add_child(scale_star)
 	move_star.connect("tween_completed",self,"star_pos_edit")
+	scale_star.connect("tween_started",self,"on_tween_started")
 	pass # Replace with function body.
 func on_correct_word():
 	if $block_button/CorrectSprite.visible:
 		$block_button/CorrectSprite.play("correct")
 		star_last_pos = $block_button/CorrectSprite.get_global_position()
-		move_star.interpolate_property($block_button/CorrectSprite, "global_position", get_global_position(), star_count_pos, 1.0 ,Tween.TRANS_BACK,Tween.EASE_IN)
+		move_star.interpolate_property($block_button/CorrectSprite, "global_position", $block_button/CorrectSprite.get_global_position(), star_count_pos, 1.0 ,Tween.TRANS_BACK,Tween.EASE_IN)
 		move_star.start()
 
+func on_tween_started(key, pos):
+	$block_button/CorrectSprite.set_visible(true)
 func star_pos_edit(key, pos):
+	$block_button/CorrectSprite.set_visible(false)
 	$block_button/CorrectSprite.set_global_position(star_last_pos)
 	scale_star.interpolate_property($block_button/CorrectSprite, "scale", Vector2(0.01, 0.01), Vector2(0.7, 0.7), 1.0 ,Tween.TRANS_LINEAR)
 	scale_star.start()
+	
 	
 func set_label(text):
 	$block_button/Label.set_text(text)
